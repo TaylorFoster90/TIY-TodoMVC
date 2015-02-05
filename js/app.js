f55ab03614c7197eadc66d3582f425159a8e7f8e
Jared Stevick <jstevick@gmail.com>
2015-02-04 19:49:39 -0500

    making baby steps

--- js/app.js
+++ js/app.js
@@ -1,21 +1,49 @@
 (function (window) {
-	'use strict';
+    'use strict';
 
-	// Your starting point. Enjoy the ride!
-	
+    // Your starting point. Enjoy the ride!
+    $(document).ready(function(){
+        $('#new-todo').on('keypress', function (e) {
+            if(e.which == 13) {
+                var newTask = $('#new-todo').val();
+                console.log(newTask);
+                var newItem = '<li><div class="view"><input class="toggle" type="checkbox"><label>' + newTask + '</label><button class="destroy"></button></div><input class="edit" value="'+ newTask +'"></li>';
+                        $('#todo-list').append(newItem);
+                    $('#new-todo').val('');
 
-	var toggleAll = $('#toggle-all');
-	toggleAll.on('click', function(){
-		$('.toggle').attr('checked', true);
-		$('li').toggleClass('completed');
-	});
+            }
+   
+        });
+    });
 
+//function delete_task() {
+//  $('button.destroy').on ('click',function(){
+//    var current_element = $(this);
+//    var id = $(this).attr('id');
+//  });
 
-
-	// 	$(document).ready(function(){
-	// 	$('.toggle').on('click', function(){
-	// 		$(this).attr('checked');
-	// 	})
-	// }); 
+//function remover(){
+//$("button.destroy").on('click',function(){
+//        $(this).remove();
+//}
+//});
+//var toggleAll = $('#toggle-all');
+//    toggleAll.on('click', function(){
+//        $('.toggle').attr('checked', true);
+//        $('li').toggleClass('completed');
+//
+//    });
 
 })(window);
+
+
+
+
+
+
+    //     $(document).ready(function(){
+    //     $('.toggle').on('click', function(){
+    //         $(this).attr('checked');
+    //     })
+    // });
+
