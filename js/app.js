8b6a865349bb09401820cc50ecbdd427ba7c1eca
Carolina Powers <carolinapoloni@gmail.com>
2015-02-20 22:11:17 -0500

    v-repeat and v-model=checked work

--- js/app.js
+++ js/app.js
@@ -1,21 +1,23 @@
 (function (window) {
 	'use strict';
 
-	// Your starting point. Enjoy the ride!
-	
-
-	var toggleAll = $('#toggle-all');
-	toggleAll.on('click', function(){
-		$('.toggle').attr('checked', true);
-		$('li').toggleClass('completed');
-	});
 
 
+    
+    
+ new Vue ({
+        el:'#todo-list',
+        data: {
+            contents: [
+                {name: "Taste Javascript", checked:true}, 
+                {name:"Buy a Unicorn", checked:false}],
+        
+            
+        },
+});   
 
-	// 	$(document).ready(function(){
-	// 	$('.toggle').on('click', function(){
-	// 		$(this).attr('checked');
-	// 	})
-	// }); 
+
 
 })(window);
+
+
