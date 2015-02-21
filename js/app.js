a6a7f4eac9ad263c9ff92de0b1ef958c1324aaa7
Jared Stevick <jstevick@gmail.com>
2015-02-21 18:01:10 -0500

    hooked up number of items remaining

--- js/app.js
+++ js/app.js
@@ -21,7 +21,9 @@ var tasks =[{
     el: '#todoapp',
     data: {
       tasks: tasks,
+      //initially set to view all items
       activeFilter: 'all',
+      //filter the items by completion status
       filters: {
         all: function () {
           return true;
@@ -35,12 +37,15 @@ var tasks =[{
      }   
     },
     methods: {
+      //remove tasks
     	remove: function(task){
         this.tasks.splice(this.tasks.indexOf(task), 1);
     	},
-    
+
+      //add some tasks
       addTask: function(tasks){
         var value  = this.newTask;
+        //prevent user from adding empty items to list
         if (!value) {
           return;
         }
@@ -48,13 +53,20 @@ var tasks =[{
         this.newTask = '';
       }
     },
-
+    
     filters: {
+      //filter the items on the list by completion
       filterTasks: function (tasks) {
-
         return tasks.filter(this.filters[this.activeFilter]);
             }
-        }
+        },
+    
+    computed: {
+      //calculate the number of active items left to do
+      itemsLeft: function () {
+      return this.tasks.filter(this.filters.active).length;
+      }
+    }    
   
   });
 
