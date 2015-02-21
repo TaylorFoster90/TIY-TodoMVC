a6162d4814ff5a4dd9aa3c2dd18eaa72d650ccdc
Jared Stevick <jstevick@gmail.com>
2015-02-21 14:08:16 -0500

    added some tasks

--- js/app.js
+++ js/app.js
@@ -1,21 +1,37 @@
 (function (window) {
-	'use strict';
+    'use strict';
 
-	// Your starting point. Enjoy the ride!
-	
+var tasks =[{
+	content: "Taste Javascript",
+	finished: true
+},
+{
+	content: 'Buy a unicorn',
+	finished: false,
+  edit: false
+},
+{
+  content: 'Hello World',
+  finished: false,
+  edit: false
+}]
 
-	var toggleAll = $('#toggle-all');
-	toggleAll.on('click', function(){
-		$('.toggle').attr('checked', true);
-		$('li').toggleClass('completed');
-	});
-
-
-
-	// 	$(document).ready(function(){
-	// 	$('.toggle').on('click', function(){
-	// 		$(this).attr('checked');
-	// 	})
-	// }); 
-
+// Your starting point. Enjoy the ride!
+  new Vue({
+    el: '#todoapp',
+    data: {
+      tasks: tasks,
+    },
+    methods: {
+    	remove: function(task){
+        this.tasks.splice(this.tasks.indexOf(task), 1);
+    	},
+    
+      addTask: function(tasks){
+        var value  = this.newTask;
+        this.tasks.push({ content: value, finished: false })
+        this.newTask = '';
+      }
+    }
+  });
 })(window);
