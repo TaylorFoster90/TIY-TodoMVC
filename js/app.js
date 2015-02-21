714c4cc3f4fdf463ccab216cbe267c78ddf7cda6
Taylor Foster <tagfoster90@gmail.com>
2015-02-21 11:22:41 -0500

    using vue

--- js/app.js
+++ js/app.js
@@ -1,47 +1,25 @@
 (function (window) {
     'use strict';
 
-    // Your starting point. Enjoy the ride!
-    $(document).ready(function(){
-        $('#new-todo').on('keypress', function (e) {
-            if(e.which == 13) {
-                var newTask = $('#new-todo').val();
-                console.log(newTask);
-                var newItem = '<li><div class="view"><input class="toggle" type="checkbox"><label>' + newTask + '</label><button class="destroy"></button></div><input class="edit" value="'+ newTask +'"></li>';
-                        $('#todo-list').prepend(newItem);
-                    $('#new-todo').val('');
-                var lengthOfToDo = $('#todo-list li').length;
-                $('#todo-count strong').text(lengthOfToDo);
+var tasks =[{
+    content: "Taste Javascript",
+    finished: true
+},
+{
+    content: 'Buy a unicorn',
+    finished: false
+}]
 
-            }
-        $('.destroy').on('click', function() {
-        $(this).parent().parent().remove();
-        var lengthOfToDo = $('#todo-list li').length;
-                $('#todo-count strong').text(lengthOfToDo);
-
-        });   
-
-            });
-//        var toggleAll = $('#toggle-all');
-//    toggleAll.on('click', function(){
-//        $('.toggle').attr('checked', true);
-//        $('li').toggleClass('completed');
-
-
-        });
-
-
-
+// Your starting point. Enjoy the ride!
+  new Vue({
+    el: '#todoapp',
+    data: {
+      todos: tasks,
+    },
+    methods: {
+        remove: function(task){
+            task.splice(this)
+        }
+    }
+  });
 })(window);
-
-
-
-
-
-
-    //     $(document).ready(function(){
-    //     $('.toggle').on('click', function(){
-    //         $(this).attr('checked');
-    //     })
-    // });
-
