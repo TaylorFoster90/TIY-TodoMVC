fec40bf175322d59122300753f78e0aa75da30f2
Carolina Powers <carolinapoloni@gmail.com>
2015-02-24 12:59:14 -0500

    saving comments for local storage

--- js/app.js
+++ js/app.js
@@ -5,10 +5,13 @@
 var tasks = [ 
     {content: "Taste Javascript",
      finished:false,
-     editing: false}, 
+     editing: false
+    }, 
+    
     {content:"Buy a Unicorn", 
      finished:false, 
-     editing:false}
+     editing:false
+    }
 ]
     
     
@@ -24,12 +27,15 @@ var tasks = [
         },
              save: function(content) {
                 content.editing=false;
-//                 SAVE
-             }
+                content.editing == this.content; 
+                //Storing(this.tasks);
+            }
     }
 });   
 
-
+//    function Storing(tasks) {
+//    localStorage.setItem('tasks', JSON.stringify('tasks').content)};
+  
 
 })(window);
 
